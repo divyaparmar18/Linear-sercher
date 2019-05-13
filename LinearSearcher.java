a7704016a3439a14e9c4d7a0c69e3d97d95e351d
divya <divya18@navgurukul.org>
2019-05-13 12:02:09 +0530

    add . getNumChecker

--- LinearSearcher.java
+++ LinearSearcher.java
@@ -1,13 +1,22 @@
 public class LinearSearcher {
     public static void main(String[] args) {
         int toBesearched = Integer.parseInt(args[0]);
-        int [] serachNumIn = {12,34,23,45,67,8,76,53,55,67,2,56,78,43,6};
-        for(int number : serachNumIn){
-            if (toBesearched == number){
-                System.out.println("yes number is present in the list");
-                return;
+        if(getNumchecker(toBesearched)) {
+            System.out.println("number  present in the list");
+            return;
+        }
+        System.out.println("number is not in the list");
+}
+
+    private static boolean getNumchecker(int number) {
+        int[] serachNumIn = {12, 34, 23, 45, 67, 8, 76, 53, 55, 67, 2, 56, 78, 43, 6};
+        for (int num : serachNumIn) {
+            if (number == num) {
+                return true;
             }
         }
-        System.out.println("not present in the list");
+        return false;
     }
+
 }
+
