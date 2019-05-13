de6c4dc18805a3b413bef1e654e53e0ed90fd2d7
divya <divya18@navgurukul.org>
2019-05-13 13:06:39 +0530

    testcases for the numbers not in list

--- test.sh
+++ test.sh
@@ -3,6 +3,10 @@ java LinearSearcher 56 > actual
 diff actual testfor56
 java LinearSearcher 34 > actual
 diff actual testfor34
+java LinearSearcher 7 > actual
+diff actual testNotInList
+java LinearSearcher 0 > actual
+diff actual testNotInList
 
 
 
