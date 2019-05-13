1376db042c91094d8bdbc3d175a062d09102685f
divya <divya18@navgurukul.org>
2019-05-13 12:36:38 +0530

    refactor . from getNumChecker to IfNumberInList

--- LinearSearcher.java
+++ LinearSearcher.java
@@ -1,14 +1,14 @@
 public class LinearSearcher {
     public static void main(String[] args) {
-        int toBesearched = Integer.parseInt(args[0]);
-        if(getNumchecker(toBesearched)) {
+        int toBeSearched = Integer.parseInt(args[0]);
+        if(IfNumberInList(toBeSearched)) {
             System.out.println("number  present in the list");
             return;
         }
         System.out.println("number is not in the list");
 }
 
-    private static boolean getNumchecker(int number) {
+    private static boolean IfNumberInList(int number) {
         int[] serachNumIn = {12, 34, 23, 45, 67, 8, 76, 53, 55, 67, 2, 56, 78, 43, 6};
         for (int num : serachNumIn) {
             if (number == num) {
