6947a908b2e0ba5377c6677420f76046bcf02750
divya <divya18@navgurukul.org>
2019-05-13 12:44:13 +0530

    refactor loop from for in loop to for i loop

--- LinearSearcher.java
+++ LinearSearcher.java
@@ -10,9 +10,9 @@ public class LinearSearcher {
 
     private static boolean IfNumberInList(int number) {
         int[] serachNumIn = {12, 34, 23, 45, 67, 8, 76, 53, 55, 67, 2, 56, 78, 43, 6};
-        for (int num : serachNumIn) {
-            if (number == num) {
-                return true;
+        for (int i = 0; i < serachNumIn.length; i++) {
+            if(serachNumIn[i] == number){
+              return true;
             }
         }
         return false;
