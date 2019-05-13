652d2c69340b51063506bdc390479330088839c9
divya <divya18@navgurukul.org>
2019-05-13 12:49:54 +0530

    show the index of number

--- LinearSearcher.java
+++ LinearSearcher.java
@@ -2,7 +2,6 @@ public class LinearSearcher {
     public static void main(String[] args) {
         int toBeSearched = Integer.parseInt(args[0]);
         if(IfNumberInList(toBeSearched)) {
-            System.out.println("number  present in the list");
             return;
         }
         System.out.println("number is not in the list");
@@ -12,7 +11,9 @@ public class LinearSearcher {
         int[] serachNumIn = {12, 34, 23, 45, 67, 8, 76, 53, 55, 67, 2, 56, 78, 43, 6};
         for (int i = 0; i < serachNumIn.length; i++) {
             if(serachNumIn[i] == number){
-              return true;
+                System.out.println("Number present in the list and it is at index " + i);
+
+                return true;
             }
         }
         return false;
